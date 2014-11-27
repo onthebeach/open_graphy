f4a250db5db3b4414429fa2a1cce6427a36796f7
James Cotterill <mail@jamescotterill.co.uk>
2014-11-27 11:29:54 +0000

    Added specs for the OpenGraphy::Fetcher

--- spec/lib/open_graphy/fetcher_spec.rb
+++ spec/lib/open_graphy/fetcher_spec.rb
@@ -2,5 +2,36 @@ require 'spec_helper'
 
 module OpenGraphy
   describe Fetcher do
+    describe '.fetch' do
+      let(:og_data) { Fetcher.fetch(uri) }
+      let(:uri) { 'http://www.imdb.com/title/tt0107048/' }
+      let(:meta_tags) { [] }
+      let(:file) { File.new('spec/support/fixtures/groundhog_day.html') }
+
+      before do
+        allow_any_instance_of(Fetcher).to receive(:open).with(uri).
+          and_return(file)
+      end
+
+      it 'has a url' do
+        expect(og_data.url).to eql('http://www.imdb.com/title/tt0107048/')
+      end
+
+      it 'has an image' do
+        expect(og_data.image).to eql('http://ia.media-imdb.com/images/M/MV5BMTU0MzQyNTExMV5BMl5BanBnXkFtZTgwMjA0Njk1MDE@._V1_.jpg')
+      end
+
+      it 'has a title' do
+        expect(og_data.title).to eql('Groundhog Day (1993)')
+      end
+
+      it 'thinks it has an image' do
+        expect(og_data.image?).to eql(true)
+      end
+
+      it 'does not have a cat' do
+        expect(og_data.cat?).to eql(false)
+      end
+    end
   end
 end
