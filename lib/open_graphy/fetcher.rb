caf68d393ad5a3cdd4b972684b955d7b6d06b53a
James Cotterill <mail@jamescotterill.co.uk>
2014-11-27 13:46:14 +0000

    clean up the fetcher

--- lib/open_graphy/fetcher.rb
+++ lib/open_graphy/fetcher.rb
@@ -9,24 +9,51 @@ module OpenGraphy
     end
 
     def fetch
-      meta_tags.each do |tag|
-        OpenGraphy.configuration.metatags.each do |metatag|
-          if tag.attr('property') =~ Regexp.new(metatag)
-            key = tag.attr('property').sub(metatag, '')
-            data.add_data(key, tag.attr('content').to_s)
-          end
-        end
+      valid_meta_tags.each do |tag|
+        data.add_data(tag.name, tag.value)
       end
+
       if !data.title
         data.add_data("title",  doc.css('title').text)
       end
+
       data
     end
 
     private
 
+    def valid_meta_tags
+      valid_meta_tags ||= meta_tags.select(&:valid?)
+    end
+
+    class MetaTag
+      def initialize(doc, meta_tag)
+        @doc, @meta_tag = doc, meta_tag
+      end
+
+      def valid?
+        tag_name
+      end
+
+      def name
+        @meta_tag.attr('property').sub(tag_name, '')
+      end
+
+      def value
+        @meta_tag.attr('content').to_s
+      end
+
+      private
+
+      def tag_name
+        OpenGraphy.configuration.metatags.find do |valid_meta_tag_name|
+          @meta_tag.attr('property') =~ Regexp.new(valid_meta_tag_name)
+        end
+      end
+    end
+
     def meta_tags
-      doc.css('//meta')
+      doc.css('//meta').map { |tag| MetaTag.new(doc, tag) }
     end
 
     def data
